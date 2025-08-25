e3838a6bd86566cbb4aec131cec4ac189ae8665c
3gic2028 <Muchsaucier@gmail.com>
2025-08-24 22:53:27 -0400

    Add files via upload

---
+++ class Main {.js
@@ -0,0 +1,18 @@
+class Main {
+  // main method placeholder (JavaScript does not use 'public static void main')
+  main(args) {
+    let str = "Hello, World!";
+    let strLength = str.length;
+    for (let i = 0; i < strLength; i++) {
+        // loop body here
+   <link rel ="stylesheet" href="css colors.css"></link>
+   if (str.toLowerCase()) {
+     // Example: print each character in lowercase
+     console.log(str[i].toLowerCase());
+   }
+    }
+  }
+    constructor(parameters) {
+        
+    }
+}
--- strings.js
+++ strings.js
@@ -15,16 +15,17 @@ let divMessage = document.getElementById("divMessage");
 
 // I am going to create an error message if the full name has either 1 character (") or more than 20
 // for the assignment, change 10 to 20
-if (fullName.length == 1 || fullName.length > 20){}
-// the user gets notified of the error but dose not get the secret message
-divMessage.innerHTML = "Invalid name entered. Please try again.";
-else if(zipCode.length !== 5){
-// if the zio code is not exactly 5 digits long give the user an error message
-else if(zipCode).Length
-// this will prevent the form from trying to submit to sever side code that we don't have in this class
- return false;
+if (fullName.length == 1 || fullName.length > 20) {
+	// the user gets notified of the error but does not get the secret message
+	divMessage.innerHTML = "Invalid name entered. Please try again.";
+	// this will prevent the form from trying to submit to server side code that we don't have in this class
+	return false;
+} else if (zipCode.length !== 5) {
+	// if the zip code is not exactly 5 digits long give the user an error message
+	divMessage.innerHTML = "Invalid zip code entered. Please try again.";
+	return false;
 }
 // if we get to this else statement, it means all of the inputs validate successfully successfully give the user a secret Message
-
 // the inputs validated successfully , so show the user a message
+divMessage.innerHTML = "Congratulations " + fullName + ". The secret word is Volleyball.";
  divMessage.innerHTML =" Congratulations" + fullName +" The secret word is Volleyball":
